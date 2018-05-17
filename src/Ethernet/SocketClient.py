99d2bdc6affbf9f5cc24895c17c87216c142fbb8
wuhailin1984 <wuhailin1984@gmail.com>
2018-05-17 13:35:19 +0200

    11

--- src/Ethernet/SocketClient.py
+++ src/Ethernet/SocketClient.py
@@ -1,4 +1,5 @@
 import socket
+import select
 
 
 class ClassSocketClient(object):
@@ -11,13 +12,19 @@ class ClassSocketClient(object):
         self.serverPort=server_port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((self.serverIP, self.serverPort))
+        self.sock.setblocking(0)
+        #self.sock.settimeout(5)
 
     def send(self,data):
 
         self.sock.send(data)
 
     def recv(self):
-        return self.sock.recv(1024)
+        ready = select.select([self.sock], [], [], 5)
+        if ready[0]:
+            return(self.sock.recv(100))
+        return  b""
+
 
     def close(self):
         self.sock.close()
